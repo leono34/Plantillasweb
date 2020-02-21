b23e143c8a94a1878e9cbdd7c9d84b8034ef8b3f
Joan Macha <isaelmacha@hotmail.com>
2020-02-21 14:32:22 -0500

    avance

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+let btnmenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+
+btnmenu.addEventListener('click', function(){
+    'use scrict';
+    menu.classList.toggle('mostrar');
+    
+});
+
+ 
